53f2557c50f0544fee1865add4ea18031343bbfb
Terence Chen <chenjienan2009@gmail.com>
2022-09-28 00:04:04 -0700

    Attack System

--- 1448.count-good-nodes-in-binary-tree.cs
+++ 1448.count-good-nodes-in-binary-tree.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Xml.Xsl.Runtime;
 /*
  * @lc app=leetcode id=1448 lang=csharp
  *
@@ -20,8 +22,34 @@
  */
 public class Solution {
     public int GoodNodes(TreeNode root) {
-        
+        int[] result = {0};
+		List<int> ancestors = new List<int>();
+
+		dfs(root, ancestors, result);
+		return result[0];
     }
+
+	public void dfs(TreeNode curNode, List<int> ancestors, int[] result)
+	{
+		if (curNode == null) return;
+
+		bool good = true;		 
+		foreach (int ancestor in ancestors)
+		{
+			if (ancestor > curNode.val)
+			{
+				good = false;
+				break;
+			}
+		}
+
+		if (good) {
+			result[0] += 1;
+		}
+		ancestor.Add(curNode.val);
+		dfs(curNode.left, ancestor, result);
+		dfs(curNode.right, ancestor, result);
+	}
 }
 // @lc code=end
 
